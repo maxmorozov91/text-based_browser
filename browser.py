73a73f320afdc38dc3aa786dab4a2601d8850c6c
Maksim Morozov <sa1ivan@mail.ru>
2020-05-12 21:20:37 +0300

    Discard the latest changes

--- browser.py
+++ browser.py
@@ -117,6 +117,3 @@ while True:
         if is_url_correct(user_input):
             show_new_page(user_input)
             download_page(user_input)
-
-def test_function():
-    print("Hello World! Hi Max!")
